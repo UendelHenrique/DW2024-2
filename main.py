82e9f8bd06f5ad74d032573b3237066f035e615a
UendelHenrique <uendel.henrique.ifro@gmail.com>
2024-12-03 10:00:24 -0400

    Método GET. - Adição do template 'login.html'. - Adição da rota "/login". - Adição da rota "/autenticar" para verificar como o método GET funciona e exibe os parâmetros na url.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app_Uendel = Flask (__name__)
 
@@ -16,6 +16,7 @@ def indice():
     return render_template("index.html", nome = nome_User)
 
 @app_Uendel.route("/usuario/<nome_usuario>;<nome_profissao>;<nome_curso>")
+@app_Uendel.route("/usuario", defaults={"nome_usario":"usuario?","nome_profissao":""})
 def usuario(nome_usuario, nome_profissao, nome_curso):
     dados_usu = {"Profissão": nome_profissao, "Curso":nome_curso}
     return render_template("usuario.html", nome = nome_usuario, dados = dados_usu)
@@ -23,6 +24,16 @@ def usuario(nome_usuario, nome_profissao, nome_curso):
 @app_Uendel.route('/<id>')
 def saudacao(id):
     return render_template('homepage_nome.html', nome=id)
+
+@app_Uendel.route("/login")
+def login():
+    return render_template("login.html")
+
+@app_Uendel.route("/autenticar", methods=['GET', 'POST'])
+def autenticar():
+    usuario = request.args.get('nome_usuario')
+    senha = request.args.get('senha')
+    return f"usuario: {usuario} e senha: {senha}"
 
 if __name__ == "__main__":
     app_Uendel.run(port = 8080)

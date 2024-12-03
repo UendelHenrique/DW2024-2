b51bcec719b001c8b982210cd95c955cbbe71c0a
UendelHenrique <uendel.henrique.ifro@gmail.com>
2024-12-03 01:05:55 -0400

    - Adicição da rota contato que renderiza o template 'contato.html'.

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 app_Uendel = Flask (__name__)
 
 @app_Uendel.route('/')
-
 def raiz():
     return render_template("homepage.html")
+
+@app_Uendel.route('/contato')
+def contato():
+    return render_template("contato.html")
 
 if __name__ == "__main__":
     app_Uendel.run(port = 8080, debug= True)

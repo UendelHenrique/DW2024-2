4db5ab726c48fc6e6ee979e5f4f8ae98629693fe
UendelHenrique <uendel.henrique.ifro@gmail.com>
2024-12-03 01:01:37 -0400

    -Adiciona rota raiz que renderiza o template 'homepage.html'. - Define o servidor para rodar na porta 8080.

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app_Uendel = Flask (__name__) #cria objeto da aplicação.
+app_Uendel = Flask (__name__)
 
-@app_Uendel.route('/') #rota para solicitação web
-@app_Uendel.route('/rota1') #rota para solicitação web
-def rota1(): #função a ser executada quando chamar rotas acima.
-    return 'Olá, turma!'
+@app_Uendel.route('/')
 
-@app_Uendel.route('/rota2')
-def rota2():
-    resposta = "<H3>Essa é outra página da rota 2 <H3>"
-    return resposta
-
-def saudacoes (nome):
-    return f'Olá, {nome}'
+def raiz():
+    return render_template("homepage.html")
 
 if __name__ == "__main__":
-    app_Uendel.run() #executa aplicação.
+    app_Uendel.run(port = 8080, debug= True)

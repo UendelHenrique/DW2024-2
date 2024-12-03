1ff4a784d456851db579cecf8209844cb1642a6f
UendelHenrique <uendel.henrique.ifro@gmail.com>
2024-12-03 00:23:38 -0400

    Alteração e adição de rotas. - Altera rota '/ola' para '/rota1'. - Adição da rota '/rota2'. - Adicionado pasta 'old' para guardar o projeto que estava sendo feito durante as aulas.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-"""
-@author: Uendel
-"""
 from flask import Flask
 
-app_Uendel = Flask (__name__)
+app_Uendel = Flask (__name__) #cria objeto da aplicação.
 
-@app_Uendel.route('/')
-@app_Uendel.route('/ola')
-
-def raiz():
+@app_Uendel.route('/') #rota para solicitação web
+@app_Uendel.route('/rota1') #rota para solicitação web
+def rota1(): #função a ser executada quando chamar rotas acima.
     return 'Olá, turma!'
 
-def saudacoes(nome):
+@app_Uendel.route('/rota2')
+def rota2():
+    resposta = "<H3>Essa é outra página da rota 2 <H3>"
+    return resposta
+
+def saudacoes (nome):
     return f'Olá, {nome}'
 
 if __name__ == "__main__":
-    app_Uendel.run(port = 8080, debug= True)
+    app_Uendel.run() #executa aplicação.

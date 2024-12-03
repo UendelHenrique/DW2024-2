582b92d7f1b7a078a6e9079af09825c4bf3e2d34
UendelHenrique <uendel.henrique.ifro@gmail.com>
2024-12-03 02:28:44 -0400

    Páginas dinâmicas via URL. - Adição da template 'homepage_nome.html'. - Adição de rota '/<id>' para a template 'homepage_nome.html' renderizar com nome informado via url. - Alteração da rota '/usuario' para a template 'usuário.html' renderizar dinamicamente com dados url.

--- main.py
+++ main.py
@@ -10,16 +10,19 @@ def raiz():
 def contato():
     return render_template("contato.html")
 
-@app_Uendel.route("/usuario")
-def dados_usuario():
-    nome_usuario="Uêndel"
-    dados_usu = {"profissao": "Estudante", "Curso": "Sistemas para internet"}
-    return render_template("usuario.html", nome = nome_usuario, dados = dados_usu)
-
 @app_Uendel.route("/index")
 def indice():
     nome_User = "Uêndel"
     return render_template("index.html", nome = nome_User)
+
+@app_Uendel.route("/usuario/<nome_usuario>;<nome_profissao>;<nome_curso>")
+def usuario(nome_usuario, nome_profissao, nome_curso):
+    dados_usu = {"Profissão": nome_profissao, "Curso":nome_curso}
+    return render_template("usuario.html", nome = nome_usuario, dados = dados_usu)
+
+@app_Uendel.route('/<id>')
+def saudacao(id):
+    return render_template('homepage_nome.html', nome=id)
 
 if __name__ == "__main__":
     app_Uendel.run(port = 8080)

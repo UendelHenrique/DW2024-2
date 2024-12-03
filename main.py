2fa48900fd9af2f807f600c39f2eb0f26841d562
UendelHenrique <uendel.henrique.ifro@gmail.com>
2024-12-02 21:26:03 -0400

    Adição da rota '/ola' e alteração de porta de acesso à aplicação.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+"""
+@author: Uendel
+"""
 from flask import Flask
 
-app_Uendel = Flask(__name__)
+app_Uendel = Flask (__name__)
 
 @app_Uendel.route('/')
+@app_Uendel.route('/ola')
+
 def raiz():
     return 'Olá, turma!'
 
-app_Uendel.run()
+def saudacoes(nome):
+    return f'Olá, {nome}'
+
+if __name__ == "__main__":
+    app_Uendel.run(port = 8080, debug= True)
